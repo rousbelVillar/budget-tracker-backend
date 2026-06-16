c469de4b0e4b92c772a1cdd57e6b17d7d0a31257
rousbelVillar <rousbel_01@hotmail.com>
2026-06-15 18:40:00 -0700

    adding user module

---
+++ budget_tracker/modules/user_module.py
@@ -0,0 +1,60 @@
+from flask import jsonify, request
+from werkzeug.utils import secure_filename
+from supabase import create_client
+from dotenv import load_dotenv
+from functools import wraps
+import uuid, os
+from budget_tracker.auth_utils import  verify_auth_token
+from budget_tracker.extensions import db
+
+class User:
+    def __init__(self):
+        load_dotenv()
+        url = os.environ.get("SUPABASE_URL")
+        key = os.environ.get("SUPABASE_KEY")
+        self.supabase = create_client(url, key)
+        self.BUCKET_NAME = "photos"
+        self.public_url = ""
+
+    def add_image(self,file,user) :
+        BUCKET_NAME = "photos"
+        load_dotenv()
+        url: str = os.environ.get("SUPABASE_URL")
+        key: str = os.environ.get("SUPABASE_KEY")
+        supabase = create_client(url, key)
+        ext = secure_filename(file.filename).rsplit('.', 1)[1].lower()
+        unique_name = f"{uuid.uuid4().hex}.{ext}"
+        
+        # Read file contents
+        contents = file.read()
+        # Upload to Supabase Storage
+        supabase.storage.from_(BUCKET_NAME).upload(unique_name, contents)
+
+        # Get public URL (string)
+        self.public_url = supabase.storage.from_(BUCKET_NAME).get_public_url(unique_name)
+
+        # Insert record into database
+        if not user.picture :
+            supabase.table(BUCKET_NAME).\
+                insert({"title": user.id, "image_url": self.public_url}).execute()
+
+@staticmethod
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = request.cookies.get("token")
+        if not token:
+            return jsonify({"message": "Not authenticated"}), 401
+        user_id = verify_auth_token(token)
+        if not user_id:
+            return jsonify({"message": "Invalid or expired session"}), 401
+        return f(user_id=user_id, *args, **kwargs)
+    return decorated_function
+
+@staticmethod
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_public_url(self):
+    return self.public_url
